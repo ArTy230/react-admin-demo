1fe568c9015574787eb66fd04ee306abbb7064d9
Minuka Weerathunga <minuka.weerathunga@itelasoft.com>
2020-10-08 01:30:13 +0530

    posts component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Admin, Resource } from 'react-admin';
+import restProvider from 'ra-data-json-server';
+import PostList from './components/PostList'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
-      </header>
-    </div>
-  );
+  return <Admin dataProvider={restProvider('http://localhost:3000')}>
+    <Resource name='posts' list={PostList} />
+  </Admin>
 }
 
 export default App;
