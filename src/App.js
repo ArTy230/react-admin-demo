b4f373ac377002527fc0f94297fef08e3739eb57
Minuka Weerathunga <minuka.weerathunga@itelasoft.com>
2020-10-08 12:51:38 +0530

    Added User Resource and it's components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import restProvider from 'ra-data-json-server';
-import PostList from './components/PostList'
-import PostCreate from './components/PostCreate'
-import PostEdit from './components/PostEdit'
+import PostList from './components/posts/PostList'
+import PostCreate from './components/posts/PostCreate'
+import PostEdit from './components/posts/PostEdit'
+
+import UserList from './components/users/UserList'
+import UserCreate from './components/users/UserCreate'
+import UserEdit from './components/users/UserEdit'
 import './App.css';
 
 function App() {
   return <Admin dataProvider={restProvider('http://localhost:3000')}>
-    <Resource name='posts' list={PostList} create={PostCreate} edit={PostEdit} />
+    <Resource
+      name='posts'
+      list={PostList}
+      create={PostCreate}
+      edit={PostEdit}
+    />
+    <Resource
+      name='users'
+      list={UserList}
+      create={UserCreate}
+      edit={UserEdit}
+    />
   </Admin>
 }
 
