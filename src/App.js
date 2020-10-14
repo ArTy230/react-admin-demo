37a0407becc4d80b94fc6c534d445921b447ad96
Alisher K <alisherkabildjanov@yahoo.com>
2020-10-14 13:47:37 -0500

    feature: created a feature to login as mod. Restricted access to users and posts ad specified in the issue

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
-import restProvider from 'ra-data-json-server';
-import PostList from './components/posts/PostList'
-import PostCreate from './components/posts/PostCreate'
-import PostEdit from './components/posts/PostEdit'
+import { Admin, Resource, usePermissions } from 'react-admin';
 
-import UserList from './components/users/UserList'
-import UserCreate from './components/users/UserCreate'
-import UserEdit from './components/users/UserEdit'
+import restProvider from 'ra-data-json-server';
+import PostList from './components/posts/PostList';
+import PostCreate from './components/posts/PostCreate';
+import PostEdit from './components/posts/PostEdit';
+
+import authProvider from './authProvider';
+import UserList from './components/users/UserList';
+import UserCreate from './components/users/UserCreate';
+import UserEdit from './components/users/UserEdit';
 import './App.css';
 
 function App() {
-  return <Admin dataProvider={restProvider('http://localhost:3000')}>
-    <Resource
-      name='posts'
-      list={PostList}
-      create={PostCreate}
-      edit={PostEdit}
-    />
-    <Resource
-      name='users'
-      list={UserList}
-      create={UserCreate}
-      edit={UserEdit}
-    />
-  </Admin>
+  return (
+    <Admin
+      dataProvider={restProvider('http://localhost:3000')}
+      authProvider={authProvider}
+    >
+      {(permissions) => [
+        <Resource
+          key='1'
+          name='posts'
+          list={PostList}
+          create={PostCreate}
+          edit={permissions === 'moderator' ? PostEdit : 'null'}
+        />,
+        <Resource
+          key='2'
+          name='users'
+          list={UserList}
+          create={permissions === 'moderator' && null}
+          edit={permissions === 'moderator' && null }
+        />,
+      ]}
+    </Admin>
+  );
 }
 
 export default App;
